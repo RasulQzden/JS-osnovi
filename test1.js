4488f07ca2d88002667c18a28dd8ac85d6d936ae
RasulQzden <113776874+RasulQzden@users.noreply.github.com>
2022-11-27 16:20:21 +0200

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,81 @@
+/*
+alert ("Найти корень ax2+bx+c=0");
+let a = +prompt("Введите а:");
+let b = +prompt("Введите b:");
+let c = +prompt("Введите c:");
+let d=(b**2 - 4 * a * c);
+if(d>=0){
+let x_1 = (-b + Math.sqrt(d) ) / (2 * a);
+let x_2 = (-b - Math.sqrt(d) )/ (2 * a);
+alert("Корень 1: " + x_1);
+alert("Корень 2: " + x_2); }
+else alert("Нет действительных корней ");
+*/
+
+/*
+let month = prompt("Номер месяца");
+switch (month) {
+    case '1':
+    case '12':
+    case '2': alert ("Зима");
+        break;
+    case '3':
+    case '4':
+    case '5': alert ("Весна");
+        break;
+    case '6':
+    case '7':
+    case '8': alert ("Лето");
+        break;        
+    case '9':
+    case '10':
+    case '11': alert ("Осень");
+            break;   
+    default: alert ("Вы ввели неверный месяц")
+        break;
+}
+*/
+
+
+let month = prompt("День недели ");
+switch (month) {   
+    case '1': alert ("Понедельник");
+        break;
+    case '2': alert ("Вторник");
+        break;
+    case '3': alert ("Среда");
+        break;        
+    case '4': alert ("Четверг");
+            break;   
+    case '5': alert ("Пятница");
+            break;      
+    case '6': alert ("Суббота");
+            break; 
+    case '7': alert ("Воскресенье");
+            break; 
+    default: alert ("Вы ввели неверный месяц")
+        break;
+}
+
+/*
+alert ("Находим значения выражений ");
+let a = +prompt("Введите а:");
+let b = +prompt("Введите b:");
+let c = +prompt("Введите c:"),x;
+x = (2*c - b + 23**0.5)/(a/4 -1);
+alert ("Первое выражение равно: " + x);
+x = (c + 4*b - 123**0.5)/(1- a/2); 
+alert ("Второе выражение равно: " + x);
+x = (2*c - b*(42**0.5))/(c+a-1);
+alert ("Третье выражение равно: " + x);
+x = ((25/c)**0.5 -b +2)/(a*a +b -1);
+alert ("Четвертое выражение равно: " + x);
+*/
+/*
+let x = +prompt("Введите x: ");
+if(x<=0) {alert ( -x);}
+    else if(x>=2) {alert (4);}
+        else {alert (x);}
+*/
+
+
